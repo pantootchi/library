f8fafba006515fc79f6c3a13cc7395a17f96720a
Pat <alfonsopatrisha@gmail.com>
2022-07-05 19:41:20 -0700

    Add modal

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-Placeholder
+const addBook = document.querySelector('.add-book');
+const shelves = document.querySelectorAll('.shelf');
+const modal = document.querySelector(".modal");
+const closeButton = document.querySelector(".close-button");
+
+// MODAL 
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+addBook.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
