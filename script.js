70c44c6ced83a908e7e407da2cf335a3d84fddb9
Pat <alfonsopatrisha@gmail.com>
2022-07-06 12:31:06 -0700

    Create book modal and generate info

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 const addButton = document.querySelector('.add-button');
 const shelves = document.querySelectorAll('.shelf');
 const modal = document.querySelector(".modal");
-const closeButton = document.querySelector(".close-button");
+const closeButton = document.querySelector(".modal .close-button");
+const bookCloseButton = document.querySelector(".book-modal .close-button");
 const addBookForm = document.getElementById("addBookForm");
+const bookModal = document.querySelector(".book-modal");
+const bookInfo = document.querySelector(".book-info");
 
 const FULL_SHELF = 22;
 
 // MODAL 
 function toggleModal() {
     modal.classList.toggle("show-modal");
+    if (modal.classList.value.includes('show-modal')) {
+        addBookForm.reset();
+    }
 }
 
 function windowOnClick(event) {
     if (event.target === modal) {
         toggleModal();
+    } else if (event.target === bookModal) {
+        closeBookModal();
     }
 }
 
 addButton.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
+bookCloseButton.addEventListener("click", closeBookModal);
 window.addEventListener("click", windowOnClick);
 
 let myLibrary = [];
 let bookCounter = 0;
+let bookElements;
 
 function Book (title, author, pages, read, id) {
     this.title = title
@@ -32,11 +42,8 @@ function Book (title, author, pages, read, id) {
     this.id = id
 }
 
-// onsubmit event occurs when a form is successfully submitted
-// same as addBookForm.onsubmit = addBookToLibrary
 addBookForm.addEventListener("submit", addBookToLibrary);
 
-// e is the reference for event object which will be passed to event handlers.
 function addBookToLibrary(e) {
     // Stops form from submitting values
     e.preventDefault();
@@ -50,8 +57,57 @@ function addBookToLibrary(e) {
     // Visually display new Book object
     displayBook(newBook);
 
+    // Create NodeList of all Book elements
+    bookElementList();
+
     // Close modal
     toggleModal();
+}
+
+function bookElementList() {
+    bookElements = document.querySelectorAll('.book');
+
+    bookElements.forEach((book) => {book.addEventListener('click', openBookModal)})
+}
+
+function openBookModal(e) {
+    // Reset Book Modal's content
+    bookInfo.textContent = '';
+    // Generate Book Modal's content
+    generateBookInfo(e.target.id);
+    // Activate Book Modal
+    bookModal.classList.add("show-modal");
+}
+
+function closeBookModal() {
+    bookModal.classList.remove("show-modal");
+}
+
+function generateBookInfo(e) {
+    for (let i = 0; i < myLibrary.length; i++) {
+        if(e == myLibrary[i].id) {
+            const title = document.createElement('h1');
+            title.textContent = myLibrary[i].title;
+
+            const author = document.createElement('h2');
+            author.textContent = `by ${myLibrary[i].author}`
+
+            const pages = document.createElement('p');
+            pages.textContent = `Pages: ${myLibrary[i].pages}`
+
+            const bookId = document.createElement('p');
+            bookId.textContent = `Book ID: ${myLibrary[i].id}`
+
+            const read = document.createElement('p');
+            read.textContent = `Read: ${myLibrary[i].read}`
+
+            const div = document.createElement('div');
+            div.classList.add('book-sub-info');
+
+            bookInfo.append(title, author, div, read);
+            div.append(pages, bookId);
+        }
+    }
 }
 
 function createBookObject() {
@@ -76,10 +132,10 @@ function displayBook(bookObj) {
     const bookElement = document.createElement('span');
     bookElement.textContent = bookObj.title;
     bookElement.classList.add("book");
-    bookElement.setAttribute('id', bookObj.id)
+    bookElement.setAttribute('id', bookObj.id);
     bookElement.style.backgroundColor = generateRGB();
 
-    // Append new Book element to the DOM
+    // Append new Book element to the document
     for (i = 0; i < shelves.length; i++) {
         if (shelves[i].children.length != FULL_SHELF) {
             shelves[i].appendChild(bookElement);
